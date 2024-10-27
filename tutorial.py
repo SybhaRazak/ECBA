ae33fdc6ddef44f2c221641d2443e292f5208609
SybhaRazak <93848421+SybhaRazak@users.noreply.github.com>
2024-10-27 16:41:57 +0800

    Update tutorial.py

--- tutorial.py
+++ tutorial.py
@@ -18,7 +18,7 @@ TARGET = st.text_input("ENTER YOUR NAME")
 MUT_RATE = st.number_input("Enter your mutation rate", value=0.10, min_value=0.0, max_value=1.0, step=0.10)
 
 # Button to calculate the progress
-if st.button("Calculate"):
+if st.button("Calculate")
   
 
 #GENES: Options from which our population would be created.
