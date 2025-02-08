586eacfda36516ca49aef6726a20baacab530b8c
SybhaRazak <93848421+SybhaRazak@users.noreply.github.com>
2025-02-08 16:35:19 +0800

    Update traffic.py

--- pages/traffic.py
+++ pages/traffic.py
@@ -7,7 +7,7 @@ from PIL import Image
 
 # Function to load the CSV file containing the labels
 def load_labels():
-    file_path = "label.csv"  # Ensure this is the correct path to your CSV file
+    file_path = "pages/labels.csv"  # Ensure this is the correct path to your CSV file
 
     # Check if the file exists
     if os.path.exists(file_path):
